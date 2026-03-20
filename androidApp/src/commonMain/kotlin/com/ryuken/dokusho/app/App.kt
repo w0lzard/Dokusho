690f562c558b90c9d28373b572622228ff0f7865
wolzard <kpsingj6@gmail.com>
2026-03-21 01:47:10 +0530

    Add Android database factory and Koin wiring

---
+++ androidApp/src/commonMain/kotlin/com/ryuken/dokusho/app/App.kt
@@ -0,0 +1,105 @@
+package com.ryuken.dokusho.app
+
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.ryuken.dokusho.book.presentation.SelectedBookViewModel
+import com.ryuken.dokusho.book.presentation.book_detail.BookDetailAction
+import com.ryuken.dokusho.book.presentation.book_detail.BookDetailScreenRoot
+import com.ryuken.dokusho.book.presentation.book_detail.BookDetailViewModel
+import com.ryuken.dokusho.book.presentation.book_list.BookListScreenRoot
+import com.ryuken.dokusho.book.presentation.book_list.BookListViewModel
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = Routes.BookGraph
+        ) {
+            navigation<Routes.BookGraph>(
+                startDestination = Routes.BookList
+            ) {
+                composable<Routes.BookList>(
+                    exitTransition = { slideOutHorizontally() },
+                    popEnterTransition = { slideInHorizontally() }
+                ) {
+                    val viewModel = koinViewModel<BookListViewModel>()
+                    val selectedBookViewModel =
+                        it.sharedKoinViewModel<SelectedBookViewModel>(navController)
+
+                    LaunchedEffect(true) {
+                        selectedBookViewModel.onSelectBook(null)
+                    }
+
+                    BookListScreenRoot(
+                        viewModel = viewModel,
+                        onBookClick = { book ->
+                            selectedBookViewModel.onSelectBook(book)
+                            navController.navigate(
+                                Routes.BookDetail(book.id)
+                            )
+                        }
+                    )
+                }
+                composable<Routes.BookDetail>(
+                    enterTransition = { slideInHorizontally { initialOffset ->
+                        initialOffset
+                    } },
+                    exitTransition = { slideOutHorizontally { initialOffset ->
+                        initialOffset
+                    } }
+                ) {
+                    val selectedBookViewModel =
+                        it.sharedKoinViewModel<SelectedBookViewModel>(navController)
+                    val viewModel = koinViewModel<BookDetailViewModel>()
+                    val selectedBook by selectedBookViewModel.selectedBook.collectAsStateWithLifecycle()
+
+                    LaunchedEffect(selectedBook) {
+                        selectedBook?.let {
+                            viewModel.onAction(BookDetailAction.OnSelectedBookChange(it))
+                        }
+                    }
+
+                    BookDetailScreenRoot(
+                        viewModel = viewModel,
+                        onBackClick = {
+                            navController.navigateUp()
+                        }
+                    )
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+private inline fun <reified T: ViewModel> NavBackStackEntry.sharedKoinViewModel(
+    navController: NavController
+): T {
+    val navGraphRoute = destination.parent?.route ?: return koinViewModel<T>()
+    val parentEntry = remember(this) {
+        navController.getBackStackEntry(navGraphRoute)
+    }
+    return koinViewModel(
+        viewModelStoreOwner = parentEntry
+    )
+}
