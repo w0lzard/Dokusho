47dec762db5fd4cb77d558330937afd77ed6fdcf
wolzard <kpsingj6@gmail.com>
2026-03-20 19:06:34 +0530

    Add book list screen with search and loading states

--- androidApp/src/commonMain/kotlin/com/ryuken/dokusho/book/presentation/book_list/components/BookSearchBar.kt
+++ androidApp/src/commonMain/kotlin/com/ryuken/dokusho/book/presentation/book_list/components/BookSearchBar.kt
@@ -1,18 +1,32 @@
 package com.ryuken.dokusho.book.presentation.book_list.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import com.ryuken.dokusho.core.presentation.DarkBlue
+import com.ryuken.dokusho.core.presentation.DesertWhite
 import com.ryuken.dokusho.core.presentation.SandYellow
 import dokusho.androidapp.generated.resources.Res
+import dokusho.androidapp.generated.resources.close_hint
 import dokusho.androidapp.generated.resources.search_hint
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
@@ -22,10 +36,16 @@ import org.jetbrains.compose.resources.stringResource
 fun BookSearchBar(
     searchQuery: String,
     onSearchQueryChange: (String) -> Unit,
-    onimeSearch: () -> Unit,
+    onImeSearch: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    OutlinedTextField(
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides TextSelectionColors(
+            handleColor = SandYellow,
+            backgroundColor = SandYellow
+        )
+    ) {
+        OutlinedTextField(
         value = searchQuery,
         onValueChange = onSearchQueryChange,
         shape = RoundedCornerShape(100),
@@ -44,7 +64,38 @@ fun BookSearchBar(
                 contentDescription = "Search",
                 tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
             )
-        }
-    )
-
+        },
+        singleLine = true,
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                onImeSearch()
+            }
+        ),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Search
+        ),
+            trailingIcon = {
+                AnimatedVisibility(visible = searchQuery.isNotBlank()) {
+                    IconButton(
+                        onClick = {
+                            onSearchQueryChange("")
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = stringResource(Res.string.close_hint),
+                            tint = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+                }
+            },
+        modifier = modifier
+            .background(
+                shape = RoundedCornerShape(100),
+                color = DesertWhite
+            )
+            .minimumInteractiveComponentSize()
+        )
+    }
 }
