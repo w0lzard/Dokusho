2af3cf530e7f43da1d0ae06a8e688a541894bbdc
wolzard <kpsingj6@gmail.com>
2026-03-25 17:50:00 +0530

    Add data models and database setup for book entities and favorites (part 2)

---
+++ androidApp/src/androidMain/kotlin/com/ryuken/dokusho/di/Modules.android.kt
@@ -0,0 +1,15 @@
+package com.ryuken.dokusho.di
+
+import com.ryuken.dokusho.book.data.database.DatabaseFactory
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.okhttp.OkHttp
+import org.koin.android.ext.koin.androidApplication
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+
+actual val platformModule: Module
+    get() = module {
+        single<HttpClientEngine> { OkHttp.create() }
+        single { DatabaseFactory(androidApplication()) }
+    }
