215b56b5226ef28e9cef36d0859c47bca954deb4
wolzard <kpsingj6@gmail.com>
2026-03-26 19:16:00 +0530

    Add data models and database setup for book entities and favorites (part 9)

--- androidApp/src/iosMain/kotlin/com/ryuken/dokusho/MainViewController.kt
+++ androidApp/src/iosMain/kotlin/com/ryuken/dokusho/MainViewController.kt
@@ -1,5 +1,11 @@
 package com.ryuken.dokusho
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.ryuken.dokusho.app.App
+import com.ryuken.dokusho.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController (
+    configure = {
+        initKoin()
+    }
+){ App() }
