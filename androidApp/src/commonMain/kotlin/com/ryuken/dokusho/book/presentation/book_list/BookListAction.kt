c51cc06eb2371c57e005eb3f9c29513532bdcbb1
wolzard <kpsingj6@gmail.com>
2026-03-26 12:00:00 +0530

    Add data models and database setup for book entities and favorites (part 7)

--- androidApp/src/commonMain/kotlin/com/ryuken/dokusho/book/presentation/book_list/BookListAction.kt
+++ androidApp/src/commonMain/kotlin/com/ryuken/dokusho/book/presentation/book_list/BookListAction.kt
@@ -3,7 +3,7 @@ package com.ryuken.dokusho.book.presentation.book_list
 import com.ryuken.dokusho.book.domain.Book
 
 sealed interface BookListAction {
-    data class onSearchQueryChange(val query: String) : BookListAction
-    data class onBookClick(val book: Book) : BookListAction
-    data class onTabSelected(val index: Int) : BookListAction
+    data class OnSearchQueryChange(val query: String) : BookListAction
+    data class OnBookClick(val book: Book) : BookListAction
+    data class OnTabSelected(val index: Int) : BookListAction
 }
