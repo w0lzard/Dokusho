226fd2f23b659b4af6f48b6465fe5b4d66fd796a
wolzard <kpsingj6@gmail.com>
2026-03-27 05:45:40 +0530

    Add desktop database factory and Koin setup

--- androidApp/src/desktopMain/kotlin/com/ryuken/dokusho/main.kt
+++ androidApp/src/desktopMain/kotlin/com/ryuken/dokusho/main.kt
@@ -1,13 +1,23 @@
+
 package com.ryuken.dokusho
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.ryuken.dokusho.app.App
+import com.ryuken.dokusho.di.initKoin
+import io.ktor.client.engine.okhttp.OkHttp
 
-fun main() = application {
+fun main() {
+    initKoin()
+    application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Dokusho",
     ) {
-        App()
+        App(
+            engine = remember { OkHttp.create() }
+        )
     }
+}
 }
